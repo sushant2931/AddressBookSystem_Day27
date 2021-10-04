9561add548dfc7a4866fd398e9e81ed5e34cef44
sushant2931 <sushantlad8888@gmail.com>
2021-10-04 18:09:16 +0530

    REFACTORED : Add New Contact

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,21 +1,32 @@
 package com.bridgelabz.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
     private ArrayList<PersonDetails> referenceBook = new ArrayList<PersonDetails>();
 
     public void addPerson() {
-
+        Scanner sc = new Scanner(System.in);
         PersonDetails person1 = new PersonDetails();
-        person1.setFirstName("Sushant");
-        person1.setLastName("Lad");
-        person1.setAddress("Kavalapur, Sangli");
-        person1.setCity("Pune");
-        person1.setState("Maharashtra");
-        person1.setPinCode(411043);
-        person1.setPhoneNumber("8830602356");
-        person1.setEmail("sushantlad@gmail.com");
+        System.out.println("Enter Person details:");
+        System.out.println("Enter firstName:");
+        person1.setFirstName(sc.next());
+        System.out.println("Enter SecondName:");
+        person1.setLastName(sc.next());
+        System.out.println("Enter Address:");
+        person1.setAddress(sc.next());
+        System.out.println("Enter City:");
+        person1.setCity(sc.next());
+        System.out.println("Enter State:");
+        person1.setState(sc.next());
+        System.out.println("Enter Pin code:");
+        person1.setPinCode(sc.nextInt());
+        System.out.println("Enter Phone nmber:");
+        person1.setPhoneNumber(sc.next());
+        System.out.println("Enter email:");
+        person1.setEmail(sc.next());
+        sc.close();
         referenceBook.add(person1);
     }
 }
