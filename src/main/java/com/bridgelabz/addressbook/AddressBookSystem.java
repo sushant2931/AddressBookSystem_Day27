9655ba06a1bfdb8a8cc4a74e2d31f3f45944ed4f
sushant2931 <sushantlad8888@gmail.com>
2021-10-04 19:30:54 +0530

    REFACTORED : Added Multiple Address Book To System

--- src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
@@ -3,14 +3,66 @@ package com.bridgelabz.addressbook;
 import java.util.Scanner;
 
 public class AddressBookSystem {
+    private static AddressBook[] addressBooks = new AddressBook[10];
+    private static String[] addressBookName = new String[10];
+    private static int  numOfBooks =0;
+
+    private boolean checkName(String name) {
+        for(int i=0;addressBooks[i]!=null;i++) {
+            if(addressBookName[i].equals(name)) return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         System.out.println("****** Welcome to address book program ! ******");
         Scanner sc = new Scanner(System.in);
-        AddressBook addressBook = new AddressBook();
+
+        AddressBook currentBook;
+        boolean exit1 = true;
+        while(exit1) {
+            System.out.println("Select option 1:Add address Book 2:open Address Book 3:exit");
+            switch(sc.nextInt()) {
+                case 1:
+                    System.out.println("Enter the address book name");
+                    String name = sc.next();
+                    currentBook  = new AddressBook();
+                    addressBooks[numOfBooks] = currentBook;
+                    addressBookName[numOfBooks] = name;
+                    numOfBooks++;
+                    break;
+                case 2:
+                    System.out.println("The Address books available :");
+                    for(int i=0;i<numOfBooks;i++) {
+                        System.out.println(addressBookName[i]);
+                    }
+                    System.out.println("Enter the name");
+                    String book = sc.next();
+                    int i =0;
+                    for(i=0;i<numOfBooks;i++) {
+                        if(addressBookName[i].equals(book)) break;
+                    }
+                    if(i == numOfBooks) {
+                        System.out.println("name Not Found");
+                        break;
+                    }
+                    currentBook = addressBooks[i];
+                    addressMenu(currentBook);
+                    break;
+                default:
+                    exit1 = false;
+
+            }
+        }
+        
+        sc.close();
+    }
+
+    private static void addressMenu(AddressBook addressBook) {
+        Scanner sc = new Scanner(System.in);
         int option = 0;
         boolean exit = true;
         while(exit) {
-            System.out.println("Select option 1: add user.  2: edit existing user.  3: display all users 4:Delete contact 5:Exit");
+            System.out.println("Select option 1: add user.  2: edit existing user.  3: display all users 4:Delete contact. 5:Switch Address Book");
             option  = sc.nextInt();
             switch(option) {
                 case 1 :
@@ -29,9 +81,9 @@ public class AddressBookSystem {
                     break;
                 default:
                     exit = false;
+
             }
             System.out.println();
         }
-        sc.close();
     }
 }
