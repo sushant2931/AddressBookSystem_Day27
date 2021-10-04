0e9b7395b5cfc1605c5bef3d177a160099537bbb
sushant2931 <sushantlad8888@gmail.com>
2021-10-04 19:15:23 +0530

    REFACTORED : Implementation Using Array

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -4,67 +4,108 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    private ArrayList<PersonDetails> referenceBook = new ArrayList<PersonDetails>();
+    private PersonDetails[] referenceBook = new PersonDetails[20];
+    private int numOfContacts = 0;
 
     public void addPerson() {
         System.out.println("Enter Person details:");
-        referenceBook.add(intake());
+
+        Scanner sc = new Scanner(System.in);
+        PersonDetails person1 = new PersonDetails();
+
+        System.out.println("Enter firstName:");
+        String name = sc.next();
+        for(int i=0; referenceBook[i]!= null;i++) {
+            if(referenceBook[i].getFirstName().equals(name)) {
+                System.out.println("the name is already taken");
+                return;
+            }
+        }
+        person1.setFirstName(name);
+        System.out.println("Enter SecondName:");
+        person1.setLastName(sc.next());
+        System.out.println("Enter Address:");
+        person1.setAddress(sc.next());
+        System.out.println("Enter City:");
+        person1.setCity(sc.next());
+        System.out.println("Enter State:");
+        person1.setState(sc.next());
+        System.out.println("Enter Pin code:");
+        person1.setPinCode(sc.nextInt());
+        System.out.println("Enter Phone nmber:");
+        person1.setPhoneNumber(sc.next());
+        System.out.println("Enter email:");
+        person1.setEmail(sc.next());
+        referenceBook[numOfContacts] = person1;
+        numOfContacts++;
     }
 
     public void editPerson(String name) {
-        PersonDetails person = null;
+        PersonDetails person = null ;
         int i = 0;
         //Check entered person name
-        while (i < referenceBook.size()) {
-            if (referenceBook.get(i).getFirstName().equals(name)) {
-                person = referenceBook.get(i);
+        while(referenceBook[i]!= null) {
+            if(referenceBook[i].getFirstName().equals(name)) {
+                person = referenceBook[i];
                 break;
             }
             i++;
         }
         // if name not present return
-        if (person == null) {
+        if(person == null) {
             System.out.println("name not found!");
             return;
         }
-        System.out.println("Changing details, Enter new details  of " + name);
-        referenceBook.add(i, intake());
+
+        System.out.println("Changing details, Enter new details  of "+name);
+        referenceBook[i] = intake();
     }
 
     public void display() {
         Scanner sc = new Scanner(System.in);
         PersonDetails person = null;
         System.out.println("Persons present in the address book:");
-        for (int i = 0; i < referenceBook.size(); i++) {
-            System.out.print(referenceBook.get(i).getFirstName() + "  ");
+        for(int i=0; referenceBook[i] != null;i++) {
+            System.out.print(referenceBook[i].getFirstName()+"  ");
         }
         System.out.println();
         System.out.println("Enter name to see details");
         String name = sc.next();
 
-        for (int i = 0; i < referenceBook.size(); i++) {
-            if (referenceBook.get(i).getFirstName().equals(name)) {
-                person = referenceBook.get(i);
+        for(int i = 0;referenceBook[i]!= null;i++) {
+            if(referenceBook[i].getFirstName().equals(name)) {
+                person = referenceBook[i];
                 break;
             }
         }
-        if (person == null) {
+        if(person == null) {
             System.out.println("name not found!");
             return;
         }
         output(person);
+
     }
 
+
     public void deletePerson(String name) {
-        for(int i=0;i<referenceBook.size();i++) {
-            if(referenceBook.get(i).getFirstName().equals(name)) {
-                referenceBook.remove(i);
-                System.out.println("Deleted details of : "+name);
-                return;
+        int i=0;
+        for(i=0;referenceBook[i]!=null;i++) {
+            if(referenceBook[i].getFirstName().equals(name)) {
+                break;
             }
         }
-        System.out.println("Name not found");
+        if(i==numOfContacts) {
+            System.out.println("Name not found");
+            return;
+        }
+        while(referenceBook[i+1]!= null) {
+            referenceBook[i] = referenceBook[i+1];
+            i++;
+        }
+        referenceBook[i] = null;
+        System.out.println("Deleted details of : "+ name);
     }
+
 
 
     public static PersonDetails intake() {
