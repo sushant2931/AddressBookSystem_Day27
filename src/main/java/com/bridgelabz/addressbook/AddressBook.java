36453fd04d71285285061f7631738596c25a630c
sushant2931 <sushantlad8888@gmail.com>
2021-10-04 18:32:49 +0530

    REFACTORED : Edit Existing Contact

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -7,9 +7,58 @@ public class AddressBook {
     private ArrayList<PersonDetails> referenceBook = new ArrayList<PersonDetails>();
 
     public void addPerson() {
+        System.out.println("Enter Person details:");
+        referenceBook.add(intake());
+    }
+
+    public void editPerson(String name) {
+        PersonDetails person = null;
+        int i = 0;
+        //Check entered person name
+        while (i < referenceBook.size()) {
+            if (referenceBook.get(i).getFirstName().equals(name)) {
+                person = referenceBook.get(i);
+                break;
+            }
+            i++;
+        }
+        // if name not present return
+        if (person == null) {
+            System.out.println("name not found!");
+            return;
+        }
+        System.out.println("Changing details, Enter new details  of " + name);
+        referenceBook.add(i, intake());
+    }
+
+    public void display() {
+        Scanner sc = new Scanner(System.in);
+        PersonDetails person = null;
+        System.out.println("Persons present in the address book:");
+        for (int i = 0; i < referenceBook.size(); i++) {
+            System.out.print(referenceBook.get(i).getFirstName() + "  ");
+        }
+        System.out.println();
+        System.out.println("Enter name to see details");
+        String name = sc.next();
+
+        for (int i = 0; i < referenceBook.size(); i++) {
+            if (referenceBook.get(i).getFirstName().equals(name)) {
+                person = referenceBook.get(i);
+                break;
+            }
+        }
+        if (person == null) {
+            System.out.println("name not found!");
+            return;
+        }
+        output(person);
+    }
+
+    public static PersonDetails intake() {
         Scanner sc = new Scanner(System.in);
         PersonDetails person1 = new PersonDetails();
-        System.out.println("Enter Person details:");
+
         System.out.println("Enter firstName:");
         person1.setFirstName(sc.next());
         System.out.println("Enter SecondName:");
@@ -26,7 +75,17 @@ public class AddressBook {
         person1.setPhoneNumber(sc.next());
         System.out.println("Enter email:");
         person1.setEmail(sc.next());
-        sc.close();
-        referenceBook.add(person1);
+        return person1;
+    }
+
+    public static void output(PersonDetails person) {
+        System.out.println("Enter firstName : " + person.getFirstName());
+        System.out.println("Enter SecondName : " + person.getLastName());
+        System.out.println("Enter Address : " + person.getAddress());
+        System.out.println("Enter City : " + person.getCity());
+        System.out.println("Enter State : " + person.getState());
+        System.out.println("Enter Pin code : " + person.getPinCode());
+        System.out.println("Enter Phone nmber : " + person.getPhoneNumber());
+        System.out.println("Enter email : " + person.getEmail());
     }
 }
--- src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookSystem.java
@@ -1,9 +1,33 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("****** Welcome to address book program ! ******");
+        Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        addressBook.addPerson();
+        int option = 0;
+        boolean exit = true;
+        while(exit) {
+            System.out.println("Select option 1: add user.  2: edit existing user.  3: display all users  4:Exit");
+            option  = sc.nextInt();
+            switch(option) {
+                case 1 :
+                    addressBook.addPerson();
+                    break;
+                case 2 :
+                    System.out.println("Enter the user name to edit");
+                    addressBook.editPerson(sc.next());
+                    break;
+                case 3:
+                    addressBook.display();
+                    break;
+                default:
+                    exit = false;
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
